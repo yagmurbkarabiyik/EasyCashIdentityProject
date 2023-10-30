098727c6fd5ec3f6be89ff24ebac7b4b1bd230b7
Yagmur Karabiyik <yagmurbkarabiyik@gmail.com>
2023-10-30 17:49:14 +0300

    entity added

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccount
+    {
+        public int Id { get; set; }
+        public string AccountNumber { get; set; }
+        public string AccountCurrency { get; set; }
+        public decimal AccountBalance { get; set; }
+        public string BankBranch { get; set; }
+
+    }
+}
+
