55179d64effb49f3e91c12209a6577b13b3d81a0
Yagmur Karabiyik <yagmurbkarabiyik@gmail.com>
2023-11-06 09:49:39 +0300

    send money

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/SendMoneysController.cs
@@ -0,0 +1,43 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using EasyCashIdentityProject.DtoLayer.Dtos.CustomerAccountDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class SendMoneysController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly ICustomerAccountProcessService _customerAccountProcessService;
+
+        public SendMoneysController(UserManager<AppUser> userManager, ICustomerAccountProcessService customerAccountProcessService)
+        {
+            _userManager = userManager;
+            _customerAccountProcessService = customerAccountProcessService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateDto createDto)
+        {
+            var context = new Context();
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            var receiverAccountNumber = context.CustomerAccounts.Where(x => x.AccountNumber == createDto.ReceiverAccountNumber)
+               .Select(y => y.Id).FirstOrDefault();
+            createDto.SenderId = user.Id;
+            createDto.ReceiverId = receiverAccountNumber;
+            createDto.ProcessDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            createDto.ProcessType = "Deneme";
+           
+           
+            return View();
+        }
+    }
+}
--- EasyCashIdentityProject.PresentationLayer/Program.cs
+++ EasyCashIdentityProject.PresentationLayer/Program.cs
@@ -1,4 +1,8 @@
+using EasyCashIdentityProject.BusinessLayer.Abstract;
+using EasyCashIdentityProject.BusinessLayer.Concrete;
+using EasyCashIdentityProject.DataAccessLayer.Abstract;
 using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using EasyCashIdentityProject.DataAccessLayer.EntityFramework;
 using EasyCashIdentityProject.EntityLayer.Concrete;
 using EasyCashIdentityProject.PresentationLayer.Models;
 
@@ -13,6 +17,10 @@ builder.Services.AddDbContext<Context>();
 builder.Services.AddIdentity<AppUser, AppRole>()
                  .AddEntityFrameworkStores<Context>()
                  .AddErrorDescriber<CustomIdentityValidator>();
+
+
+builder.Services.AddScoped<ICustomerAccountDal, EfCustomerAccountProcessDal>();
+builder.Services.AddScoped<IAccountProcessService, AccountProcessManager>();  
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
